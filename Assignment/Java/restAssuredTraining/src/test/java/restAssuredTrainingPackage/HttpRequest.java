7174a48807dcbfc7c53608dbe11560a9db445ca2
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2024-11-14 21:26:14 +0530

    Add files via upload

---
+++ Assignment/Java/restAssuredTraining/src/test/java/restAssuredTrainingPackage/HttpRequest.java
@@ -0,0 +1,22 @@
+package restAssuredTrainingPackage;
+
+import org.testing.annotations.Test;
+
+/*
+given()
+	content type, set cookies, add auth, add param, set headers info etc...
+
+when()
+	get, post,put, delete
+	
+then()
+	validate status code, extract response, extract headers cookies and response body...
+*/
+
+public class HttpRequest {
+	
+	@Test
+	void getRequest() {
+		
+	}
+}
