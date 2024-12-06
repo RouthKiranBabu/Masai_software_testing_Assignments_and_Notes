9f6df8b9e51b1e4ee5659d6afab138ccf25e601b
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2024-12-06 09:50:32 +0530

    Add files via upload

---
+++ Lectures/Selenium/YouTube/Class_27_WebDriver Waiting Methods/MavenProject/src/main/java/PackOne/Class_2.java
@@ -0,0 +1,38 @@
+package PackOne;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Class_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		Fluent wait declaration
+		Wait<WebDriver> mywait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(30))
+				.pollingEvery(Duration.ofSeconds(5))
+				.ignoring(NoSuchElementException.class);
+		
+		driver.manage().window().maximize();
+		
+		WebElement txtusername = mywait.until(new Function<WebDriver, WebElement>(){
+			public WebElement apply(WebDriver driver) {
+				return driver.findElement(By.xpath("//input[@name='username']"));
+			}
+		});
+		txtusername.sendKeys("Admin");
+	}
+
+}
