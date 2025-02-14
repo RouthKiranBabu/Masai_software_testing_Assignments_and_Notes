468ecd596647dcd273060663945df3b8b20a20a2
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2025-02-14 17:56:48 +0530

    Add files via upload

--- Lectures/PlayWright/Part07_Assertions/Part01.spec.js
+++ Lectures/PlayWright/Part07_Assertions/Part01.spec.js
@@ -11,7 +11,28 @@ test('AssertionsTest', async ({page}) => {
     const logoElement = await page.locator('.header-logo')  
     await expect(logoElement).toBeVisible()
     
-    const searchStoreBox = await page.locator('#small-searchterms')
+    const searchStoreBox = await page.locator("//input[@id = 'small-searchterms']")
     await expect(searchStoreBox).toBeEnabled()
 
+    const maleRadioButton = await page.locator('#gender-male')  // For radio button
+    await maleRadioButton.click()                               // Select Radio Button
+    await expect(maleRadioButton).toBeChecked()
+
+    const newsCheckBox = await page.locator('#Newsletter')
+    await expect(newsCheckBox).toBeChecked()
+    
+    const regbutton = await page.locator('#register-button')
+    await expect(regbutton).toHaveAttribute('type', 'submit')
+
+    // To check exact text
+    await expect(await page.locator('.page-title h1')).toHaveText('Register')
+    // To check partial text
+    await expect(await page.locator('.page-title h1')).toContainText('ister')
+
+    const emailinput = await page.locator('#Email')
+    await emailinput.fill('test@demo.com')
+    await expect(emailinput).toHaveValue('test@demo.com')
+
+    const options = await page.locator('.form-fields')
+    await expect(options).toHaveCount(4)
 })
