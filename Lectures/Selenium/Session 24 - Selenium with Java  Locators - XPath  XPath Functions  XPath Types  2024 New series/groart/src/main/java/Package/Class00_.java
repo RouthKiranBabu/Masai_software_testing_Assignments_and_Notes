2a57d8626d5809352ae44102f708e11ae98afd5a
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2025-02-19 11:47:36 +0530

    Add files via upload

---
+++ Lectures/Selenium/Session 24 - Selenium with Java  Locators - XPath  XPath Functions  XPath Types  2024 New series/groart/src/main/java/Package/Class00_.java
@@ -0,0 +1,25 @@
+package Package;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class00_ {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.opencart.com/");
+		
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("T-Shirts");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@placeholder='Search']")).clear();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("New T-Shirts");
+		Thread.sleep(2000);
+		
+		driver.quit();
+	}
+
+}
