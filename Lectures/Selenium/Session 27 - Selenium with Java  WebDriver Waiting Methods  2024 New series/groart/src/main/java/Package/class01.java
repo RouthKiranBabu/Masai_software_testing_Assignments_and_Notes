0f2e40c7626fd6037e513e7cd1cfce9f18d849c3
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2025-02-20 20:48:47 +0530

    Add files via upload

---
+++ Lectures/Selenium/Session 27 - Selenium with Java  WebDriver Waiting Methods  2024 New series/groart/src/main/java/Package/class01.java
@@ -0,0 +1,54 @@
+package Package;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class class01 {
+
+	public static void main(String[] args) throws InterruptedException{
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		WebDriverWait wdw = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		WebElement user = wdw.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='username']")));
+		user.sendKeys("Admin");
+		
+		WebElement pwd = wdw.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
+		pwd.sendKeys("admin123");
+		
+		WebElement btn = wdw.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
+		btn.click();
+		
+		Thread.sleep(3000);
+		driver.close();
+		
+		/*Conditions
+		 * alertIsPresent()
+		 * elementSelectionStateToBe()
+		 * elementToBeClickable()
+		 * elementToBeSelected()
+		 * frameToBeAvailableAndSwitchTolt()
+		 * presenceOfAllElementsLocated()
+		 * presenceOfElementLocated()
+		 * textToBePresentInElement()
+		 * textToBePresentInElementLocated()
+		 * textToBePresentInElementValue()
+		 * titleIs()
+		 * titleContains()
+		 * visibilityOf()
+		 * visibilityOfAllElements()
+		 * visibilityOfAllElementsLocatedBy()
+		 * visibilityOfElementLocated()*/
+	}
+
+}
